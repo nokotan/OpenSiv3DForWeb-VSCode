a01e08e837aa52e1338c650e9ddc5eed6e58e55e
nokotan <kamenokonokotan@gmail.com>
2021-01-06 15:21:09 +0900

    Accept Release URL

--- UpdateOpenSiv3D.sh
+++ UpdateOpenSiv3D.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl -L -o OpenSiv3D.tgz https://github.com/nokotan/OpenSiv3D/releases/download/v0.4.3.3-web/OpenSiv3D-wasm.tgz
+curl -L -o OpenSiv3D.tgz $1
 
 # Extract & Overwrite
 tar -xvf OpenSiv3D.tgz
